79b557024f243784a4115de301c937014fda4a4e
Leonardo Luigi Pepe <leo2852.info@gmail.com>
2021-10-27 13:16:41 +0200

    trash code removed. Comments added

--- Java-Hanoi/Matrix/Main.java
+++ Java-Hanoi/Matrix/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int N = 4;
-    private static final int MAX_MOVES = N * N - 1;
-    private static int[][] towers;
+    private static final int N = 4; //number of plates
+    private static final int LEAST_MOVES = N * N - 1; //number of moves you could end the game with
+    private static int[][] towers; //array containing the three towers
 
     public static void main(String args[]) {
         initTowers();
         System.out.println("Come on!!! Use numbers between 1 and 3");
         Scanner t = new Scanner(System.in);
-        int begin;
-        int end;
+        int begin; //tower which you want to move the plate from
+        int end; //tower which you are moving the plate on
         int moves = 0;
         do {
             printTowers();
@@ -22,15 +22,15 @@ public class Main {
                 end = t.nextInt() - 1;
                 switchTower(begin, end);
                 moves++;
-            } catch (InvalidTowerException e) {
+            } catch (InvalidTowerException e) { //exception created by me to simply handle the cases which the players writes the wrong inputs
                 System.out.println("Invalid tower input");
-            } catch (InvalidSwitchException e) {
+            } catch (InvalidSwitchException e) {//exception created by me to simply handle the cases which the players makes an incorrect move
                 System.out.println("Invalid switch");
             }
 
         } while (!hasWon());
         t.close();
-        if (moves <= MAX_MOVES) {
+        if (moves <= LEAST_MOVES) {
             System.out.println("YOU DID AN EXCELLENT WORK!!!");
             printTowers();
             System.out.println("YOU DID AN EXCELLENT WORK!!!");
@@ -39,12 +39,15 @@ public class Main {
             printTowers();
             System.out.println("YOU WON!!!");
             System.out.println("Moves you did: "+ moves);
-            System.out.println("With "+ N+" plates you could finish the game with "+MAX_MOVES);
+            System.out.println("With "+ N+" plates you could finish the game with "+LEAST_MOVES);
             System.out.println("Maybe try to do better next time!");
         }
     }
 
     private static void initTowers() {
+        //with this method you init the array
+        //the first column will be filled with numbers from 0 to N-1
+        //the other two columns are filled with -1
         towers = new int[N][3];
         for (int i = 0; i < N; i++)
             towers[i][0] = i;
@@ -55,6 +58,7 @@ public class Main {
     }
 
     private static String toStringNSpazi(int n) {
+        //method which returns a string made of a precise (n) amount of blank spaces
         String s = "";
         for (int i = 0; i < n; i++)
             s += " ";
@@ -62,6 +66,9 @@ public class Main {
     }
 
     private static String toStringDisco(int r, int c) {
+        //method which returns a string made of numbers corrisponding a disk
+        //if what you are analizing is a -1 value it will return a string with 
+        //n spaces on the left, a '|' character in the middle and n spaces on the right
         String s = "";
         if (towers[r][c] == -1) {
             String spazi = toStringNSpazi(N - 1);
@@ -74,13 +81,13 @@ public class Main {
             s += spazi;
             for (int i = 0; i < l; i++)
                 s += towers[r][c];
-            // s+='_';
             s += spazi;
         }
         return s;
     }
 
     private static String toStringRiga(int r) {
+        //return as a string a row of plates
         String s = "";
         for (int c = 0; c < 3; c++) {
             s += toStringDisco(r, c);
@@ -90,6 +97,7 @@ public class Main {
     }
 
     private static String toStringTowers() {
+        //returns as a string all the towers
         String s = "";
         for (int i = 0; i < N; i++) {
             System.out.println(toStringRiga(i));
@@ -98,48 +106,41 @@ public class Main {
     }
 
     private static void printTowers() {
+        //method which print all the towers
         System.out.println(toStringTowers());
     }
 
     private static void switchTower(int begin, int end) throws InvalidTowerException, InvalidSwitchException {
-        if ((begin < 0 || begin > 2) || (end < 0 || end > 2))
+        //method which switch the plate
+        if ((begin < 0 || begin > 2) || (end < 0 || end > 2) || (begin==end))
             throw new InvalidTowerException();
+        //if the inputs are out of bounds or are the same it launches an Exception
         int plateBeginId = scanTower(begin);
         int plateEndId = scanTower(end);
 
-        /*
-         * if (towers[plateBeginId][begin] < towers[plateEndId][end] ||
-         * towers[plateBeginId][begin] > -1) { towers[plateEndId -
-         * (towers[plateEndId][end] != -1 ? 1 : 0)][end] = towers[plateBeginId][begin];
-         * towers[plateBeginId][begin] = -1; } else { throw new
-         * InvalidSwitchException(); }
-         */
-        if (towers[plateEndId][end] == -1) {
+        if (towers[plateEndId][end] == -1) {//if the tower is empty it simply put the plate
             towers[plateEndId][end] = towers[plateBeginId][begin];
             towers[plateBeginId][begin] = -1;
-        } else if (towers[plateEndId][end] > towers[plateBeginId][begin]) {
+        } else if (towers[plateEndId][end] > towers[plateBeginId][begin]) {//if the tower is not empty and the plate we want to put on is not larger than the plate below then it puts the plate
             towers[plateEndId - 1][end] = towers[plateBeginId][begin];
             towers[plateBeginId][begin] = -1;
-        } else {
+        } else { //if none of the previews conditions is satisfied it launches an Exception
             throw new InvalidSwitchException();
         }
 
     }
 
     private static int scanTower(int towerId) {
+        //returns the id of the first plate that you encounter in a tower
         int lvl = 0;
-        /*
-         * for (; lvl < N && towers[lvl][towerId] == -1; lvl++) ; //if (lvl == N)
-         * //lvl--; lvl = lvl==0?0:lvl-1;
-         */
         while ((lvl < N - 1 && towers[lvl][towerId] == -1)) {
             lvl++;
         }
-
         return lvl;
     }
 
     private static boolean hasWon() {
+        //method which tells you if you won or not (you need to fullfill one of the other two columns)
         return towers[0][1] == 0 || towers[0][2] == 0;
     }
 
